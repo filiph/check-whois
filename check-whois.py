25da0a563aa9f45a978d6d7f4d2f2fd23a19261e
Filip Hracek <filip.hracek@gmail.com>
2015-07-02 10:45:14 +0200

    Log instead of just printing to console. Also, catch whois errors

--- check-whois.py
+++ check-whois.py
@@ -39,11 +39,15 @@ Options:
 
 """
 
-
-import sys, os, codecs, time
+import sys
+import codecs
+import time
+import logging
 import getopt
 import subprocess
+
 import filipUtils
+
 
 def main(argv):
     
@@ -118,8 +122,19 @@ def main(argv):
     else:
         usage()
         sys.exit(2)
-    
-    print("Opening files and starting...\n")
+
+    log_formatter = logging.Formatter(
+        "%(asctime)s [%(levelname)-5.5s]  %(message)s"
+    )
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG if debug else logging.INFO)
+    log_file_handler = logging.FileHandler("check-whois.log")
+    log_file_handler.setFormatter(log_formatter)
+    logger.addHandler(log_file_handler)
+    log_console_handler = logging.StreamHandler(sys.stdout)
+    logger.addHandler(log_console_handler)
+
+    logger.info("Opening files and starting.")
     
     try:
         infile = codecs.open(input_file_path, encoding='utf-8')
@@ -127,23 +142,16 @@ def main(argv):
             infile.readline()
 
     except IOError:
-        print("Could not open file '" + input_file_path + "'. Exiting.")
+        logger.critical("Could not open file '%s'. Exiting.", input_file_path)
         sys.exit(1)
         
     try:
         outfile = open(output_file_path, 'a')
     except IOError:
-        print("Could not open file '" + output_file_path +
-              "' for writing. Exiting.")
+        logger.critical("Could not open file '%s' for writing. Exiting.",
+                         output_file_path)
         sys.exit(1)
         
-    if debug:
-        try:
-            debugfile = open("check-whois.log", 'w')
-        except IOError:
-            print("Could not open file 'check-whois.log' for writing. Exiting.")
-            sys.exit(1)
-    
     for line in infile:
         # this line does: " svatý mikuláš\n" --> "svatymikulas"
         domain_name = filipUtils.removeSpaces(
@@ -152,42 +160,43 @@ def main(argv):
                         )).encode('ascii', 'ignore').strip()
         
         # filtering out too short and too long domains
-        if len(domain_name) <= max_domain_length and \
-                        len(domain_name) >= min_domain_length:
+        if min_domain_length <= len(domain_name) <= max_domain_length:
             domain_tld = domain_name + "." + tld
+            logger.info("Trying %s", domain_tld)
             
             output = ""
-            waitSeconds = 1
+            wait_seconds = 1
             
             while True:
-                time.sleep(1)  # Netiquette.
+                time.sleep(wait_seconds)  # Netiquette.
                 # this line calls the system shell line `whois svatymikulas.cz
                 # and gets stdout
-                output = subprocess.Popen(["whois", domain_tld],
-                                          stdout=subprocess.PIPE)\
-                    .communicate()[0]
-                
-                if debug:
-                    debugfile.write(output)
-                    debugfile.flush()
-                
+                output, err = subprocess.Popen(
+                    ["whois", domain_tld], stdout=subprocess.PIPE
+                ).communicate()
+
+                if err:
+                    logger.error(err)
+
+                logger.debug(output)
+
                 # find out if whois threw the 'connection limit' error
                 limit_index = output.find(whois_connection_limit_string[tld])
-                if limit_index is -1:
+                if err or limit_index != -1:
+                    # Exponentially increasing the wait period.
+                    wait_seconds *= 2
+                    # We got the 'connection limit exceeded' or another error.
+                    # Waiting some time.
+                    logger.debug("... waiting %s seconds", str(wait_seconds))
+                    time.sleep(wait_seconds)
+                else:
                     # it appears we have the whole thing
                     break
-                else:
-                    # we got the 'connection limit exceeded' error. Waiting some
-                    # time.
-                    print("... waiting " + str(waitSeconds) + " seconds ...")
-                    time.sleep(waitSeconds)
-                    # exponentially increasing the wait period
-                    waitSeconds *= 2
 
             error_index = output.find(whois_not_found_string[tld])
             if error_index is not -1:
-                # we have a nonexistent domain on our hands
-                print(domain_tld + " is free!")
+                # We have a nonexistent domain on our hands.
+                logger.info("- %s is free", domain_tld)
                 outfile.write(domain_tld + "\n")
                 # this is here in case you want to do `tail -f xyz.txt` in the
                 # terminal
@@ -196,14 +205,11 @@ def main(argv):
             else:
                 # it seems the domain is already registered: let's just print
                 # it out
-                print(domain_tld)
+                logger.debug("%s already registered", domain_tld)
                 
     infile.close()
     outfile.close()
     
-    if debug:
-        debugfile.close()
-
 
 if __name__ == "__main__":
     main(sys.argv[1:])
